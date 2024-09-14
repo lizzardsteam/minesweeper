2dd2a544aab95271bd8745de3e0084fbfa17c20e
Stefan Dimov <lizzardsteam@gmail.com>
2024-09-15 00:04:48 +0300

    Express added in order to serve files without getting CORS messages.

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express")
+const path = require("path")
+const cors = require("cors")
+const app = express()
+const port = 3000
+
+app.use('/public', express.static('./bin'))
+app.use(cors())
+app.get('/', (req, res) => {
+    let indexFilePath = path.join(__dirname, "./ui/index.html")
+    res.sendFile(indexFilePath)
+})
+
+app.listen(port, () => {
+    console.log(`Minesweeper server listening on port: ${port}`)
+})
