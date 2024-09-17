147ce0105be72cb3a31b5660aed4c0a049e2b231
Stefan Dimov <lizzardsteam@gmail.com>
2024-09-17 20:49:54 +0300

    Button text color set to black explicitly.

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import Minesweeper, { GAME_DEFAULT_MSG } from "./Entities/Minesweeper.js"
 import BoardDrawer from "./Utils/BoardDrawer.js"
 
-const boardSize: number = 6
+const boardSize: number = 5
 const totalBombs: number = 6
 
 const game = new Minesweeper(boardSize, totalBombs, gameOverCallback)
