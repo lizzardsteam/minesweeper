05c58db845377eb93ea97eda547953c42614bfda
Stefan Dimov <lizzardsteam@gmail.com>
2024-10-19 21:59:14 +0300

    User class changed to store date as string.

--- server/src/models/users/User.ts
+++ server/src/models/users/User.ts
@@ -3,13 +3,13 @@ export default class User {
     public username: string
     public email: string
     public passwordHash: string
-    public createdAt: Date
+    public createdAt: string
 
     public constructor(id: string, username: string, email: string, passwordHash: string) {
         this.id = id
         this.username = username
         this.email = email
         this.passwordHash = passwordHash
-        this.createdAt = new Date()
+        this.createdAt = new Date().toString()
     }
 }
