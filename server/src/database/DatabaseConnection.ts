a15493fea106b05a1e141074cb8e3fa784f99a1f
Stefan Dimov <lizzardsteam@gmail.com>
2024-10-19 22:00:00 +0300

    DatabaseConnection file changed to export db.

--- server/src/database/DatabaseConnection.ts
+++ server/src/database/DatabaseConnection.ts
@@ -1,1 +1,6 @@
-export default {}
+import { sqliteConfig } from "../config/sqlite";
+import Database from "better-sqlite3";
+
+const db = new Database(sqliteConfig.path, { verbose: console.log }); 
+
+export default db
